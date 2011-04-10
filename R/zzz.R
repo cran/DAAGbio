d8bcdf09d2cdf6bada0f304cd288601417b82a97
John Maindonald <john.maindonald@anu.edu.au>
2011-04-10 00:00:00 +0000

    version 0.6

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.First.lib <-
+function (...)  {
+  cat("\nAs of version 0.6 of DAAGbio, the '.spot' files in the",
+      "\ninst/doc subdirectory are stored in a compressed format.",
+      "\nThe vignette marray-notes.pdf demonstrates the use of",
+      "\nthe limma function read.maimages() to read these files.",
+      "\nThis requires R version 2.10.0 or later.\n")
+}
